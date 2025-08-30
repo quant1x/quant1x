78d2be7200099ab67862666241f2b36765af9cf6
wangfeng <wangfengxy@sina.cn>
2025-08-31 05:06:19 +0800

    删除废弃的代码

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     data_files=[
         # ('xtquant', ['xtquant/xtdata.ini', 'xtquant/xtdata.log4cxx']),
