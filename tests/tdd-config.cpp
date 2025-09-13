8d9b0b158c626849d8dea6ac506f4ebeef81c8f2
wangfeng <wangfengxy@sina.cn>
2025-09-13 11:16:36 +0800

    优化yaml配置自动反序列化

--- tests/tdd-config.cpp
+++ tests/tdd-config.cpp
@@ -1,6 +1,8 @@
 #include <quant1x/test/test.h>
 #include <quant1x/runtime/config.h>
 
+#include "quant1x/runtime/core.h"
+
 // 从YAML加载配置
 config::TraderParameter LoadConfigFromYAML(const std::string& filename) {
     config::TraderParameter config;
@@ -13,8 +15,7 @@ config::TraderParameter LoadConfigFromYAML(const std::string& filename) {
     return config;
 }
 
-TEST_CASE("q2x-config-basic", "[config]") {
-
+TEST_CASE("config-basic", "[config]") {
     const std::string& cfg_filename = config::config_filename();
     std::cout << cfg_filename << std::endl;
     config::TraderParameter traderParameter{};
@@ -25,10 +26,19 @@ TEST_CASE("q2x-config-basic", "[config]") {
     std::cout << traderParameter << std::endl;
 }
 
-TEST_CASE("q2x-config-base", "[config]") {
+TEST_CASE("config-base", "[config]") {
     // 加载配置
     auto const & config = config::TraderConfig();
 
     // 打印配置
     std::cout << *config << std::endl;
+}
+
+TEST_CASE("config-data", "[config]") {
+    runtime::global_init();
+    // 加载配置
+    auto const & config = config::global_config();
+
+    // 打印配置
+    std::cout << config << std::endl;
 }
