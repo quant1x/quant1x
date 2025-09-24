a5f0a7be9b2723c7a54ea0b368ec1f08a7e366d4
wangfeng <wangfengxy@sina.cn>
2025-09-24 09:07:25 +0800

    删除废弃的代码，quant1x不收录实验性代码

--- examples/num/add_amd64.go
+++
@@ -1,4 +0,0 @@
-package num
-
-//go:noescape
-func sse2Add(a, b, c *float64, n int)
--- examples/sectors/s1.py
+++
@@ -1,7 +0,0 @@
-from quant1x import cache, exchange
-
-block_list = cache.block_list()
-print(block_list)
-#881075 贵金属
-list = cache.get_sector_constituents('880675')
-print(list)
