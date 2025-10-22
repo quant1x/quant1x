fddbea8b67c0d65274ce2898846a39db132c6789
wangfeng <wangfengxy@sina.cn>
2025-10-23 07:11:07 +0800

    调整测试代码

--- tests/tdd-klines.cpp
+++ tests/tdd-klines.cpp
@@ -1,8 +1,8 @@
 #include <quant1x/test/test.h>
 #include <quant1x/datasets/kline_raw.h>
 
-#include "quant1x/datasets/kline.h"
-#include "quant1x/datasets/kline_minute.h"
+#include <quant1x/datasets/kline.h>
+#include <quant1x/datasets/kline_minute.h>
 
 TEST_CASE("download-kline-raw", "[datasets]") {
     std::string code = "sz300773";
