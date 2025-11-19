213c604eeb5be0216bb7f2fcaf71837635df6377
wangfeng <wangfengxy@sina.cn>
2025-11-19 10:34:27 +0800

    go: logger包路径和c++统一为log

--- quant1x/log/logger.go
+++ quant1x/log/logger.go
@@ -1,20 +1,20 @@
-package logging
+package log
 
 import (
-    "log"
+	"log"
 )
 
 // A tiny project-local logging wrapper to provide leveled logging for Go code in this repo.
 // This is intentionally minimal: it forwards to the standard library logger but prefixes level tags.
 
 func Infof(format string, v ...interface{}) {
-    log.Printf("[INFO] "+format, v...)
+	log.Printf("[INFO] "+format, v...)
 }
 
 func Debugf(format string, v ...interface{}) {
-    log.Printf("[DEBUG] "+format, v...)
+	log.Printf("[DEBUG] "+format, v...)
 }
 
 func Errorf(format string, v ...interface{}) {
-    log.Printf("[ERROR] "+format, v...)
+	log.Printf("[ERROR] "+format, v...)
 }
