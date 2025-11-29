d08f1ec2426b629b119d7fecbe6f79b243515eeb
wangfeng <wangfengxy@sina.cn>
2025-11-30 06:22:51 +0800

    rust: 对齐homedir源文件名

--- quant1x/std/mod.rs
+++ quant1x/std/mod.rs
@@ -2,5 +2,5 @@ pub mod buffer;
 pub use buffer::BinaryStream;
 pub mod error;
 pub use error::DeserializeError;
-pub mod system;
-pub use system::homedir;
+pub mod homedir;
+pub use homedir::homedir;
