62a383b8d875b4e06922fb4980814f822c3b888b
wangfeng <wangfengxy@sina.cn>
2025-12-01 11:25:30 +0800

    python: 优化yaml解析

--- quant1x/encoding/yaml.py
+++ quant1x/encoding/yaml.py
@@ -1,52 +1,101 @@
 # -*- coding: UTF-8 -*-
 
 import os
-
 import yaml
-from icecream import ic
+from types import SimpleNamespace
+
+try:
+    from yaml import CSafeLoader as Loader, CSafeDumper as Dumper
+except ImportError:
+    from yaml import SafeLoader as Loader, SafeDumper as Dumper
 
 
-class DictToClass(object):
+def to_namespace(data):
     """
-    将字典准换为 class 类型
+    递归将字典转换为 SimpleNamespace，支持属性访问
     """
+    if isinstance(data, dict):
+        return SimpleNamespace(**{k: to_namespace(v) for k, v in data.items()})
+    elif isinstance(data, list):
+        return [to_namespace(i) for i in data]
+    return data
 
-    @classmethod
-    def to_object(cls, _obj):
-        _obj_ = type('new', (object,), _obj)
-        [setattr(_obj_, key, cls.to_object(value)) if isinstance(value, dict) else setattr(_obj_, key, value) for
-         key, value in _obj.items()]
-        return _obj_
+
+def load(path, to_object=False):
+    """
+    读取YAML文件
+    :param path: 文件路径
+    :param to_object: 是否转换为对象(SimpleNamespace)
+    :return: 字典或对象
+    """
+    if not os.path.exists(path):
+        return None
+
+    with open(path, 'r', encoding='utf-8') as f:
+        data = yaml.load(f, Loader=Loader)
+
+    if to_object:
+        return to_namespace(data)
+    return data
+
+
+def dump(data, path):
+    """
+    保存为YAML文件
+    :param data: 数据
+    :param path: 文件路径
+    """
+    # 注意：SimpleNamespace 不能直接被 yaml dump，如果需要保存对象，需先转回 dict
+    # 这里假设 data 是 dict, list 或基础类型
+    with open(path, 'w', encoding='utf-8') as f:
+        yaml.dump(data, f, Dumper=Dumper, allow_unicode=True)
 
 
 class ReadConfigFiles(object):
-    def __init__(self):
-        """
-        获取当前工作路径
-        """
-        self.works_path = os.path.dirname(os.path.realpath(__file__))
+    """
+    带缓存的配置文件读取类
+    """
+    _caches = {}  # path -> {'mtime': float, 'data': dict}
 
     @classmethod
-    def open_file(cls):
+    def cfg(cls, path, item=None):
         """
-        读取当前工作目录下cfg.yaml文件内容，并返回字典类型
-        :return:
+        调用该方法获取需要的配置，带缓存机制
+        :param path: 配置文件路径
+        :param item: 配置项名称
+        :return: SimpleNamespace 对象或具体值
         """
-        return yaml.load(
-            open(os.path.join(cls().works_path, r'cfg.yaml'), 'r', encoding='utf-8').read(), Loader=yaml.FullLoader
-        )
+        if not path or not os.path.exists(path):
+            return None
 
-    @classmethod
-    def cfg(cls, item=None):
-        """
-        调用该方法获取需要的配置，item如果为None，返回则是全部配置
-        :param item:
-        :return:
-        """
-        return DictToClass.to_object(cls.open_file().get(item) if item else cls.open_file())
+        # 检查文件修改时间，如果有变化则重新加载
+        try:
+            mtime = os.path.getmtime(path)
+            cache = cls._caches.get(path)
+
+            if cache is None or mtime > cache['mtime']:
+                data = load(path)
+                cls._caches[path] = {'mtime': mtime, 'data': data}
+            else:
+                data = cache['data']
+        except Exception:
+            return None
+
+        if not data:
+            return None
+
+        if item:
+            val = data.get(item)
+            return to_namespace(val) if isinstance(val, (dict, list)) else val
+
+        return to_namespace(data)
 
 
 if __name__ == '__main__':
-    cfg = ReadConfigFiles.cfg()
-    # 测试输出
-    ic(cfg.host.group)
+    try:
+        from icecream import ic
+        # 示例：需要传入具体路径
+        # cfg = ReadConfigFiles.cfg('path/to/config.yaml')
+        pass
+    except ImportError:
+        pass
