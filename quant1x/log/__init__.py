346fb0ecfd8e71f140e89e764f1d8eed28566519
wangfeng <wangfengxy@sina.cn>
2025-12-02 13:19:58 +0800

    python: 调整logger目录

--- quant1x/log/__init__.py
+++ quant1x/log/__init__.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 """
 @Project : quant1x
-@Package : quant1x.io
+@Package : quant1x.log
 @File    : __init__.py
 @Author  : wangfeng
 @Date    : 2025/9/15 17:01
-@Desc    : I/O
+@Desc    : 日志模块
 """
 
 from .logger import __logger as logger
