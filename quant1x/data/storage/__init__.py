c9076c94af276dc05bfe7d342d52a3c4d4808074
wangfeng <wangfengxy@sina.cn>
2026-02-04 16:33:07 +0800

    python: 文件存储抽象类

--- quant1x/data/storage/__init__.py
+++ quant1x/data/storage/__init__.py
@@ -1,5 +1,7 @@
+from .storage import FileStorage
 from .csv import csv_to_slice, slice_to_csv
 __all__ = [
+    "FileStorage", # 文件存储接口
     "csv_to_slice", # 从 CSV 加载为 dataclass 实例列表
     "slice_to_csv", # 将 dataclass 实例列表保存为 CSV
 ]
--- quant1x/data/storage/storage.py
+++ quant1x/data/storage/storage.py
@@ -1,34 +1,45 @@
 from abc import ABC, abstractmethod
+from typing import Any, List, Type, TypeVar, Generic, overload
 
-from typing import Any
+from .csv import csv_to_slice, slice_to_csv
+from quant1x.types import T
+from quant1x.data.timestamp import Timestamp
 
-class FileStorage(ABC):
+class FileStorage(ABC, Generic[T]):
+    """文件存储接口（专用于单一 dataclass 类型）"""
+    
+    def __init__(self, data_type: Type[T]) -> None:
+        self._data_type = data_type
+        self._file_name = self.file_name()
     
     @abstractmethod
-    def load(self, filepath: str) -> Any:
-        """加载数据"""
-        raise NotImplementedError("Subclass of FileStorage must implement `load` method")
+    def file_name(self) -> str:
+        """返回文件名"""
+        raise NotImplementedError("Subclass must implement `file_name`")
     
     @abstractmethod
-    def save(self, filepath: str, data: Any) -> None:
+    def should_initialize(self, timestamp: Timestamp = Timestamp.now()) -> bool:
+        raise NotImplementedError("Subclass must implement `should_initialize`")
+    
+    @abstractmethod
+    def should_update(self, timestamp: Timestamp = Timestamp.now()) -> bool:
+        raise NotImplementedError("Subclass must implement `should_update`")
+    
+    @abstractmethod
+    def update(self) -> None:
+        """更新数据（无参，因为类型已固定）"""
+        raise NotImplementedError("Subclass must implement `update`")
+    
+    def load(self) -> List[T]:
+        """加载数据（无需传 cls，类型已知）"""
+        return csv_to_slice(self._file_name, self._data_type)
+    
+    def save(self, data: List[T]) -> None:
         """保存数据"""
-        raise NotImplementedError("Subclass of FileStorage must implement `save` method")
+        slice_to_csv(self._file_name, data)
     
-    def update(self, filepath: str, data: Any) -> None:
-        """更新数据"""
-        raise NotImplementedError("Subclass of FileStorage must implement `update` method")
-    
-    def checkout(self, filepath: str) -> Any:
-        """
-        从文件存储中检出指定文件
-        
-        Args:
-            filepath (str): 需要检出的文件路径
-        
-        Returns:
-            Any: 返回检出的文件内容或对象
-        
-        Raises:
-            NotImplementedError: 如果子类未实现此方法
-        """
-        raise NotImplementedError("Subclass of FileStorage must implement `checkout` method")
+    def checkout(self) -> List[T]:
+        """检出数据（自动更新 + 加载）"""
+        if self.should_initialize() or self.should_update():
+            self.update()
+        return self.load()
